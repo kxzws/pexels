8bfb82c84bfe27471666c55fc51c2e8ded5efbcc
Alena Bulanova <kxzws54@gmail.com>
2022-07-11 12:27:43 +0300

    fix: fix CORS policy

--- src/api/apiService.ts
+++ src/api/apiService.ts
@@ -10,6 +10,7 @@ const axiosInst = axios.create({
   timeout: requestTimeout,
   headers: {
     Authorization: `${process.env.REACT_APP_API_KEY}`,
+    'Access-Control-Allow-Origin': '*',
   },
 });
 
